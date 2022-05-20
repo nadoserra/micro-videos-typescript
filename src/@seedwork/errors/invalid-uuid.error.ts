37c8fdb113518eda10b2105b00d9712d497d742c
Rinaldo Serra <rinaldoserra@gmail.com>
2022-05-20 19:30:52 +0000

    feat: continuacao da implementacao
    - Detalhe1\n-Detalhe2

---
+++ src/@seedwork/errors/invalid-uuid.error.ts
@@ -0,0 +1,9 @@
+
+
+export default class InvalidUuidError extends Error{
+
+    constructor(message?:string) {
+        super(message || 'ID must be a valid UUID');
+        this.name = 'InvalidUuidError'  
+    }
+}
